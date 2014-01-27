83dc1c4b3994bb8e9aecb2ceac615d7a7ab87377
Lukasz Wikierski <lukasz.wikierski@allegro.pl>
2014-01-27 15:25:07 +0100

    Bank interface (transfers)

--- Bank1/src/pl/training/bank/Bank.java
+++ Bank1/src/pl/training/bank/Bank.java
@@ -1,6 +1,13 @@
 package pl.training.bank;
 
+import java.math.BigDecimal;
+
 public interface Bank {
 
+    void payInCashToAccount(String toAccountNumber, BigDecimal amount) throws BankException;
+
+    void payOutCashFromAccount(String fromAccountNumber, BigDecimal amount) throws BankException;
+
+    void transferCash(String fromAccountNumber, String toAccountNumber, BigDecimal amount) throws BankException;
 
 }
