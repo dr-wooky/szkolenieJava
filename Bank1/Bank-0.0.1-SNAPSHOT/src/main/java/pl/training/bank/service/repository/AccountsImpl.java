0f20851f97bbc28cf57cbee7ea29d92c20a277c2
Lukasz Wikierski <lukasz.wikierski@allegro.pl>
2014-01-30 10:11:26 +0100

    Configuring application as .war

--- Bank1/Bank-0.0.1-SNAPSHOT/src/main/java/pl/training/bank/service/repository/AccountsImpl.java
+++ Bank1/Bank-0.0.1-SNAPSHOT/src/main/java/pl/training/bank/service/repository/AccountsImpl.java
@@ -2,7 +2,6 @@ package pl.training.bank.service.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 import pl.training.bank.entity.Account;
 
 @Repository
